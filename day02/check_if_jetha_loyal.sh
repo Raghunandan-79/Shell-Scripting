ab1ba3cc95f5a494ffb4d253d459659f1a756932
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-03-14 10:16:57 +0000

    Added elif condition in script

--- day02/check_if_jetha_loyal.sh
+++ day02/check_if_jetha_loyal.sh
@@ -5,8 +5,12 @@ This is just for infotainment purpose
 disclaimer
 
 read -p "Jethalal ne mudke kisko dekha: " bandi
+read -p "Jethalal ka pyaar percentage: " pyaar
 
 if [[ $bandi == "Daya bhabhi"  ]];
+then
+	echo "Jethalal is loyal"
+elif [[ $pyaar -ge 100 ]];
 then
 	echo "Jethalal is loyal"
 else
