e6284ffb7c02e2d7ce1102c83c9f56f42f9ee0a9
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-03-14 14:40:44 +0530

    Added day 1 scripts

---
+++ day01/hello.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This is script for TWS
+
+echo "TWS: Hello, Dosto"
+echo "Learner: Devops wale bhaiya, hum to video dekhenge"
+echo "TWS: Toh dosto like b krdo"
+echo "Learner: Like maangne ka tareeka thoda casual hai"
