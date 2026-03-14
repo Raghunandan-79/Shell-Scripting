25dc14f59bd53e02f707bbd641ac2a9c0144e286
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-03-14 10:09:35 +0000

    Jethalal loyalty check script

---
+++ day02/check_if_jetha_loyal.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+<<disclaimer
+This is just for infotainment purpose
+disclaimer
+
+read -p "Jethalal ne mudke kisko dekha: " bandi
+
+if [[ $bandi == "Daya bhabhi"  ]];
+then
+	echo "Jethalal is loyal"
+else
+	echo "Jethalal is not loyal"
+fi
